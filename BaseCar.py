2e13a1358094df3c459a42905b43ce7690516258
sch4ufel <95682939+sch4ufel@users.noreply.github.com>
2021-12-09 11:29:42 +0100

    Data-Logging für SensorCar fertiggestellt
    - Objektinitialisierungen von Init in jeweilige Methode verschoben (BaseCar und SensorCar)
    - Einstellbarer Abstand bei check_Obstacle
    - SensorCar neue Methoden (set_..._sensor, drive_..._sensor, get_distance_uss, write_logfile, write_logfile_new_run)
    - Fahrparcours optimiert (100ms Wartezeiten zwischen Befehlen verschoben in BaseCar Methoden)

--- BaseCar.py
+++ BaseCar.py
@@ -1,6 +1,9 @@
 import basisklassen as bk
 import time
 import numpy as np
+import csv
+import os.path
+import datetime
 
 ### Hauptklasse
 class BaseCar:
@@ -16,76 +19,133 @@ class BaseCar:
     """
     
     def __init__(self, turning_offset, forward_A, forward_B):
-        self.TURNING_OFFSET = turning_offset
-        self.FORWARD_A = forward_A
-        self.FORWARD_B = forward_B
-        self.fw = bk.Front_Wheels(turning_offset=self.TURNING_OFFSET)
-        self.bw = bk.Back_Wheels(forward_A=self.FORWARD_A, forward_B=self.FORWARD_B)
-        self.DIRECTION = 0
+        self._turning_offset = turning_offset
+        self._forward_A = forward_A
+        self._forward_B = forward_B
+        self._direction = 0
+        self._speed = 0
+        self._steering_angle = 0
         self.drive_stop()
         self.set_steering_angle(90)
    
     def set_steering_angle(self, turn_angle = 0):
-        self.fw.turn(turn_angle)
+        fw = bk.Front_Wheels(turning_offset=self._turning_offset)
+        self._steering_angle = fw.turn(turn_angle)
+        time.sleep(.1)
 
     def get_steering_angle(self):
-        self.fw.get_angles()
-
+        return self._steering_angle
+        
     def drive_forward(self, speed = 0):
-        self.bw.speed = speed
-        self.bw.forward()
-        self.DIRECTION = 1
+        bw = bk.Back_Wheels(forward_A=self._forward_A, forward_B=self._forward_B)
+        bw.speed = speed
+        bw.forward()
+        time.sleep(.1)
+        self._direction = 1
 
     def drive_backward(self, speed = 0):
-        self.bw.speed = speed
-        self.bw.backward()
-        self.DIRECTION = -1
+        bw = bk.Back_Wheels(forward_A=self._forward_A, forward_B=self._forward_B)
+        bw.speed = speed
+        bw.backward()
+        time.sleep(.1)
+        self._direction = -1
 
     def drive_stop(self):
-        self.bw.stop()
-        self.DIRECTION = 0
+        bw = bk.Back_Wheels(forward_A=self._forward_A, forward_B=self._forward_B)
+        bw.stop()
+        time.sleep(.1)
+        self._direction = 0
 
     def get_speed(self):
-        speed = self.bw.speed
-        return speed
+        bw = bk.Back_Wheels(forward_A=self._forward_A, forward_B=self._forward_B)
+        self._speed = bw.speed
+        return self._speed
 
     def get_direction(self):
-        return self.DIRECTION
+        return self._direction
 
 ### Unterklasse
 class SensorCar(BaseCar):
     """
     Diese Klasse ist eine Unterklasse der Klasse BaseCar und erbt alle Methoden der Klasse BaseCar.
-    Zusätzlich hat die Klasse die Methode:
-    - check_Obstacle um mittels Ultraschallsensor Hindernisse zu erkennen und anzuhalten.
+    Zusätzlich hat die Klasse die Methoden:
+    - check_Obstacle: Mittels Ultraschallsensor Hindernisse erkennen und anzuhalten.
+    - write_logfile: Zeitstempel, Geschwindigkeit, Fahrtrichtung, Lenkwinkel und Abstand Hindernis in CSV-Datei speichern
+    - write_logfile_new_run:
     """
     def __init__(self, turning_offset, forward_A, forward_B):
         super().__init__(turning_offset, forward_A, forward_B)
-        self.uss = bk.Ultrasonic(preparation_time=0.01, impuls_length=0.00001, timeout=0.05)
-        self.uss.stop()
+        self._distance = 0
+        
 
-    def check_obstacle(self):
+        if os.path.isfile("Log_SensorCar_USS.csv"):
+            print("Das Log-File für den Ultraschallsensor existiert bereits!")
+        else:
+            writer = csv.writer(open("Log_SensorCar_USS.csv", "w", newline=''))
+            writer.writerow(["Zeit", "Geschwindigkeit", "Fahrtrichtung", "Lenkwinkel", "Abstand Hindernis"])
+
+    def set_steering_angle_sensor(self, turn_angle):
+        super().set_steering_angle(turn_angle)
+        self.write_logfile()
+
+    def drive_forward_sensor(self, speed):
+        super().drive_forward(speed)
+        self.write_logfile()
+
+    def drive_backward_sensor(self, speed):
+        super().drive_backward(speed)
+        self.write_logfile()
+
+    def drive_stop_sensor(self):
+        super().drive_stop()
+        self.write_logfile()
+
+    def check_obstacle(self, distance_limit = 0):
+
         while True:
-            
-            distance = self.uss.distance()
 
-            if distance < 0:
+            self.write_logfile()
+
+            self.uss = bk.Ultrasonic(preparation_time=0.01, impuls_length=0.00001, timeout=0.05)
+            self._distance = self.uss.distance()
+
+            if self._distance < 0:
                 print("Keine plausiblen Ultraschallwerte. Messung wird wiederholt.")
                 continue
-            elif distance >= 0 and distance <= 5:
-                print("Der Abstand zum Hindernis ist kleiner als 5 cm. Das Auto wird angehalten.")
+            elif self._distance >= 0 and self._distance <= distance_limit:
+                print("Der Abstand zum Hindernis ist kleiner als " + str(distance_limit) + " cm. Das Auto wird angehalten.")
                 super().drive_stop()
+                self.write_logfile()
+                self.uss.stop()
                 break
-            elif distance > 5:
+            elif self._distance > distance_limit:
                 continue
 
             time.sleep(.1)
+
+    def get_distance_uss(self):
+        return self._distance
+
+    def write_logfile(self):
+
+        timestamp = datetime.datetime.now()
+        speed = self.get_speed()
+        direction = self. get_direction()
+        steering_angle = self.get_steering_angle()
+        distance = self.get_distance_uss()
+
+        writer = csv.writer(open("Log_SensorCar_USS.csv", "a", newline=''))
+        writer.writerow([timestamp, speed, direction, steering_angle, distance])
+
+    def write_logfile_new_run(self):
+        writer = csv.writer(open("Log_SensorCar_USS.csv", "a", newline=''))
+        writer.writerow(["New run", "###", "###", "###", "###"])
                  
 
 car = BaseCar(30,1,1)
 sensorCar = SensorCar(30,1,1)    
 
-fahrparcours = 3
+fahrparcours = 4
 
 if fahrparcours == 1:
 
@@ -93,7 +153,6 @@ if fahrparcours == 1:
 
     print("Das Auto fährt 3 Sekunden vorwärts.")
     car.set_steering_angle(90)
-    time.sleep(.1)
     car.drive_forward(50)
     time.sleep(3)
 
@@ -103,7 +162,6 @@ if fahrparcours == 1:
 
     print("Das Auto fährt 3 Sekunden rückwärts.")
     car.set_steering_angle(90)
-    time.sleep(.1)
     car.drive_backward(50)
     time.sleep(3)
 
@@ -116,7 +174,6 @@ elif fahrparcours == 2:
 
     print("Das Auto fährt 1 Sekunden vorwärts.")
     car.set_steering_angle(90)
-    time.sleep(.1)
     car.drive_forward(50)
     time.sleep(1)
 
@@ -126,19 +183,16 @@ elif fahrparcours == 2:
 
     print("Das Auto wartet 1 Sekunde.")
     car.drive_stop()
-    time.sleep(.1)
     car.set_steering_angle(90)
     time.sleep(1)
 
     print("Das Auto fährt 8 Sekunden mit vollem Lenkeinschlag im Uhrzeigersinn rückwärts.")
     car.set_steering_angle(135)
-    time.sleep(.1)
     car.drive_backward(50)
     time.sleep(8)
 
     print("Das Auto fährt 1 Sekunden rückwärts.")
     car.set_steering_angle(90)
-    time.sleep(.1)
     car.drive_backward(50)
     time.sleep(1)
 
@@ -146,38 +200,34 @@ elif fahrparcours == 2:
     car.drive_stop()
 
 elif fahrparcours == 3:
-
+    sensorCar.write_logfile_new_run()
     print("Das Auto fährt Fahrparcours 3.")
 
     print("Das Auto fährt solange vorwärts bis es auf ein Hindernis trifft.")
-    sensorCar.set_steering_angle(90)
-    time.sleep(.1)
-    sensorCar.drive_forward(50)
-    sensorCar.check_obstacle()
+    sensorCar.set_steering_angle_sensor(90)
+    sensorCar.drive_forward_sensor(50)
+    sensorCar.check_obstacle(10)
 
 elif fahrparcours == 4:
-
+    sensorCar.write_logfile_new_run()
     print("Das Auto fährt Fahrparcours 4.")
 
     for i in range(3):
 
         print("Das Auto fährt solange vorwärts bis es auf ein Hindernis trifft.")
-        sensorCar.set_steering_angle(90)
-        time.sleep(.1)
-        sensorCar.drive_forward(50)
-        sensorCar.check_obstacle()
+        sensorCar.set_steering_angle_sensor(90)
+        sensorCar.drive_forward_sensor(50)
+        sensorCar.check_obstacle(10)
     
         print("Hindernis ausweichen.")
-        sensorCar.set_steering_angle(45)
-        time.sleep(.1)
-        sensorCar.drive_backward(30)
-        time.sleep(2)
-        sensorCar.set_steering_angle(135)
-        time.sleep(.1)
-        sensorCar.drive_forward(50)
-        time.sleep(0.5)
-        sensorCar.drive_stop()
-        sensorCar.set_steering_angle(90)
+        sensorCar.set_steering_angle_sensor(45)
+        sensorCar.drive_backward_sensor(30)
+        time.sleep(1.5)
+        sensorCar.set_steering_angle_sensor(135)
+        sensorCar.drive_forward_sensor(30)
+        time.sleep(1.5)
+        sensorCar.drive_stop_sensor()
+        sensorCar.set_steering_angle_sensor(90)
                 
 else:
     
