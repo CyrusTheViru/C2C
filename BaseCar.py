034e356d3cd3789fda1d05789197c7978c011bbb
nelle90 <43440899+nelle90@users.noreply.github.com>
2021-12-15 15:58:12 +0100

    docstring neu und Webbrowser

--- BaseCar.py
+++ BaseCar.py
@@ -1,4 +1,7 @@
 # coding=utf-8
+""" Autonomens Fahren mit Sunfounder PiCar-S """
+
+
 from os import abort
 import basisklassen as bk
 import time
@@ -6,18 +9,20 @@ import numpy as np
 import csv
 import os.path
 import datetime
+import webbrowser
 
 ### Hauptklasse
 class BaseCar:
     """
-    Diese Klasse ruft die Klassen und Methoden der Datei basisklassen.py aufund behinhaltet folgende Methoden :
-    - set_steering_angle(int): Setzt Lenkwinkel
-    - get_steering_angle(): Rückgabe des aktuellen Lenkwinkels
-    - drive_forward(int): Vorwärtsfahren mit übergebener Geschwindigkeit
-    - drive_backward(int): Rückwärtsfahren mit übergebener Geschwindigkeit
-    - drive_stop(): Anhalten
-    - get_speed(): Rückgabe der aktuellen Geschwindigkeit
-    - get_direction(): Rückgabe der aktuellen Fahrtrichtung (1: vorwärts, 0: Stillstand, -1 Rückwärts)
+     Diese Klasse ruft die Klassen und Methoden der Datei basisklassen.py auf. Zusätzlich wird im init der Antrieb abgeschaltet und die Lenkung auf die Nullposition gefahren.
+
+    :param turning_offset: setzt den Lenkwinkeloffset in der _INIT_
+    :type turning_offset: Integer
+    :param forward_A: setzt die Drehrichtung Fahrzeugspezifisch für das erste Rad. Die Umschaltung laeuft mit dem Wechsel 0 zu 1.
+    :type forward_A: Integer
+    :param forward_B: setzt die Drehrichtung Fahrzeugspezifisch für das erste Rad. Die Umschaltung laeuft mit dem Wechsel 0 zu 1.
+    :type forward_B: Integer
+
     """
     
     def __init__(self, turning_offset, forward_A, forward_B):
@@ -27,20 +32,35 @@ class BaseCar:
         self._forward_B = forward_B
         self._direction = 0
         self._speed = 0
-        self._steering_angle = 0#
+        self._steering_angle = 0
         self._fw = bk.Front_Wheels(turning_offset=self._turning_offset)
         self._bw = bk.Back_Wheels(forward_A=self._forward_A, forward_B=self._forward_B)
         self.drive_stop()
         self.set_steering_angle(90)
    
     def set_steering_angle(self, turn_angle = 0):
+        """Setzt den Lenkwinkel
+
+        :param turn_angle: setzen von dem Ziellenkwinkel defaults to 0
+        :type turn_angle: int, optional
+        """
         self._steering_angle = self._fw.turn(turn_angle)
         time.sleep(.1)
 
     def get_steering_angle(self):
+        """Rückgabe des aktuellen Lenkwinkels
+
+        :return: Lenkwinkel in 0.5 Grad
+        :rtype: int
+        """
         return self._steering_angle
         
     def drive_forward(self, speed = 0):
+        """Vorwärtsfahren mit übergebener Geschwindigkeit
+
+        :param speed: Zielgeschwindigkeit, defaults to 0
+        :type speed: int, optional
+        """
         self._bw.speed = speed
         self._bw.forward()
         time.sleep(.1)
@@ -51,6 +71,11 @@ class BaseCar:
             self._direction = 1
 
     def drive_backward(self, speed = 0):
+        """Rückwärtsfahren mit übergebener Geschwindigkeit
+
+        :param speed: [Zielgeschwindigkeit], defaults to 0
+        :type speed: int, optional
+        """
         self._bw.speed = speed
         self._bw.backward()
         time.sleep(.1)
@@ -61,25 +86,39 @@ class BaseCar:
             self._direction = -1
 
     def drive_stop(self):
+        """Stoppt die Fahrt
+        """
         self._bw.stop()
         time.sleep(.1)
         self._speed = 0
         self._direction = 0
 
     def get_speed(self):
+        """Rückgabe der aktuellen Geschwindigkeit
+
+        :return: aktuelle Geschwindikeit
+        :rtype: int
+        """
         return self._speed
 
     def get_direction(self):
+        """Rückgabe der aktuellen Fahrtrichtung (1: vorwärts, 0: Stillstand, -1 Rückwärts)
+
+        :return: Fahrtrichtung  
+        :rtype: int
+        """
         return self._direction
 
 ### Unterklasse
 class SensorCar(BaseCar):
     """
-    Diese Klasse ist eine Unterklasse der Klasse BaseCar und erbt alle Methoden der Klasse BaseCar.
-    Zusätzlich hat die Klasse die Methoden:
-    - check_Obstacle: Mittels Ultraschallsensor Hindernisse erkennen und anzuhalten.
-    - write_logfile: Zeitstempel, Geschwindigkeit, Fahrtrichtung, Lenkwinkel und Abstand Hindernis in CSV-Datei speichern
-    - write_logfile_new_run:
+     Diese Klasse ist eine Unterklasse der Klasse BaseCar und erbt alle Methoden der Klasse BaseCar.
+    :param turning_offset: setzt den Linkwinkeloffset in der _INIT_
+    :type turning_offset: Integer
+    :param forward_A: setzt die Drehrichtung Fahrzeigspezifisch für das erste Rad. Die umschaltung laeuft mit dem wechsel 0 zu 1.
+    :type forward_A: Integer
+    :param forward_B: setzt die Drehrichtung Fahrzeigspezifisch für das erste Rad. Die umschaltung laeuft mit dem wechsel 0 zu 1.
+    :type forward_B: Integer
     """
     def __init__(self, turning_offset, forward_A, forward_B):
         super().__init__(turning_offset, forward_A, forward_B)
@@ -94,22 +133,44 @@ class SensorCar(BaseCar):
             writer.writerow(["Zeit", "Geschwindigkeit", "Fahrtrichtung", "Lenkwinkel", "Abstand_Hindernis", "Infrarot_Analogdaten", "Infrarot_Digitalwerte"])
 
     def set_steering_angle_sensor(self, turn_angle):
+        """Funktion von der Class BaseCar, Setzt den Lenkwinkel und logt den zusätzlich
+
+        :param turn_angle: setzen von dem Ziellenkwinkel defaults to 0
+        :type turn_angle: int, optional
+        """
         super().set_steering_angle(turn_angle)
         self.write_logfile()
 
     def drive_forward_sensor(self, speed):
+        """Funktion von der Class BaseCar, Vorwärtsfahren mit übergebener Geschwindigkeit und logt dies
+
+        :param speed: Zielgeschwindigkeit, defaults to 0
+        :type speed: int, optional
+        """
         super().drive_forward(speed)
         self.write_logfile()
 
     def drive_backward_sensor(self, speed):
+        """Funktion von der Class BaseCar, Rückwärtsfahren mit übergebener Geschwindigkeit und logt dies
+
+        :param speed: [Zielgeschwindigkeit], defaults to 0
+        :type speed: int, optional
+        """
         super().drive_backward(speed)
         self.write_logfile()
 
     def drive_stop_sensor(self):
+        """Funktion von der Class BaseCar, Stoppt die Fahrt und logt dies
+        """
         super().drive_stop()
         self.write_logfile()
 
     def check_obstacle(self, distance_limit = 0):
+        """Mittels Ultraschallsensor Hindernisse erkennen und anzuhalten.
+
+        :param distance_limit: setzt die Distance zum anhalten, defaults to 0
+        :type distance_limit: int, optional
+        """
 
         while True:
 
@@ -133,20 +194,38 @@ class SensorCar(BaseCar):
             time.sleep(.1)
 
     def get_distance_uss(self):
+        """Liest den aktuellen Abstand zum Hinderniss aus und liefert diesen zurück.
+
+        :return: Distance zum Hinderniss
+        :rtype: int
+        """
         self.uss = bk.Ultrasonic(preparation_time=0.01, impuls_length=0.00001, timeout=0.05)
         self._distance = self.uss.distance()
         return self._distance
 
     def get_ir_analog(self):
+        """Erfassung analoger Daten vom Infrarotsensor
+
+        :return: Analoge Werte von dem Infrarotsensoren
+        :rtype: Liste von 5 int
+        """
         self._irAnalog = self._ir.read_analog()
         return self._irAnalog
 
     def get_ir_digital(self):
+        """Erfassung digitaler Daten vom Infrarotsensor
+
+        :return: Digitale Werte von dem Infrarotsensoren
+        :rtype: Liste von 5 int
+        """
         self._irDigital = self._ir.read_digital()
         return self._irDigital
 
     def write_logfile(self):
-
+        """
+        Speichert die Werte für Zeitstempel, Geschwindigkeit, Fahrtrichtung, Lenkwinkel, Abstand Hindernis, 
+        Analoge und Digitale Werte von den Intrarotsensoren in eine CSV-Datei
+        """
         timestamp = datetime.datetime.now().strftime("%H:%M:%S.%f")[:-3]
         speed = self.get_speed()
         direction = self. get_direction()
@@ -160,12 +239,16 @@ class SensorCar(BaseCar):
         writer.writerow([timestamp, speed, direction, steering_angle, distance, ir_data, ir_digital])
 
     def write_logfile_new_run(self):
+        """ Schreibt das aktuelle Datum in die Logdatei.
+        """
         timestamp = datetime.datetime.now().strftime("%d.%m.%Y")
-        writer = csv.writer(open("Log_SensorCar_USS.csv", "a", newline=''))
-        writer.writerow([timestamp, "###", "###", "###", "###", "###"])  
+        writer = csv.writer(open("Log_SensorCar_USS_IR.csv", "a", newline=''))
+        writer.writerow([timestamp, "###", "###", "###", "###", "###"]) 
 
+""" Fahrtrecken 1 - 6 """
 
 def func_fahrparcour1():
+    """ 1. Das Auto fährt 3 Sekunden vorwärts 2. Das Auto wartet 1 Sekunde 3. Das Auto fährt 3 Sekunden rückwärts """
     import config as conf
     car = BaseCar(conf.turning_offset, conf.forward_A, conf.forward_B)
     print("Das Auto fährt Fahrparcours 1.")
@@ -188,6 +271,8 @@ def func_fahrparcour1():
     car.drive_stop()
 
 def func_fahrparcour2():
+    """ 1. Das Auto fährt 1 Sekunden vorwärts 2. Das Auto fährt 8 Sekunden mit vollem Lenkeinschlag im Uhrzeigersinn vorwärts 3. Das Auto wartet 1 Sekunde
+    4. Das Auto fährt 8 Sekunden mit vollem Lenkeinschlag im Uhrzeigersinn rückwärts 5. Das Auto fährt 1 Sekunden rückwärts """
     import config as conf
     car = BaseCar(conf.turning_offset, conf.forward_A, conf.forward_B)
     print("Das Auto fährt Fahrparcours 2.")
@@ -221,6 +306,7 @@ def func_fahrparcour2():
     car.drive_stop()
 
 def func_fahrparcour3():
+    """ 1. Das Auto fährt solange vorwärts bis es auf ein Hindernis trifft """
     import config as conf
     sensorCar = SensorCar(conf.turning_offset, conf.forward_A, conf.forward_B)
     
@@ -233,6 +319,11 @@ def func_fahrparcour3():
     sensorCar.check_obstacle(10)
 
 def func_fahrparcour4(anzahl_hindernisse = 3):
+    """  
+     1. Das Auto fährt solange vorwärts bis es auf ein Hindernis trifft 2. Das Auto weicht ingesammt 3 x einem Hindernis aus 
+    :param anzahl_hindernisse: Anzahl der zu erkennenden Hindernisse im Parcour. Nach dieser Anzahl wird die Fahrt gestoppt.
+    :type anzahl_hindernisse: int
+    """
     import config as conf
     sensorCar = SensorCar(conf.turning_offset, conf.forward_A, conf.forward_B)
 
@@ -258,6 +349,7 @@ def func_fahrparcour4(anzahl_hindernisse = 3):
 
 
 def func_fahrparcour5():
+    """ 1. Das Auto fährt der markierten Linie entlang 2. Das Auto stoppt wenn die markierte Linie endet """
     import config as conf
     sensorCar = SensorCar(conf.turning_offset, conf.forward_A, conf.forward_B)
     print("Das Auto fährt Fahrparcours 5.")
@@ -292,6 +384,7 @@ def func_fahrparcour5():
 
 
 def func_fahrparcour6():
+    """ 1. Das Auto fährt einer markierten Linie entlang 2. Das Auto stoppt wenn die markierte Linie endet oder auf ein Hindernis trifft """
     import config as conf
     sensorCar = SensorCar(conf.turning_offset, conf.forward_A, conf.forward_B)
     print("Das Auto fährt Fahrparcours 6.")
@@ -331,7 +424,7 @@ def func_fahrparcour6():
     sensorCar.drive_stop_sensor()
 
 def setIRCalibration():
-
+    """ Initialle Kalibrierung der Infrarot Sensoren """
     #Initialle Kalibrierung der Infrarot Sensoren
     ir = bk.Infrared()
     ir.cali_references()
@@ -345,6 +438,7 @@ def setIRCalibration():
     print(" Die Referenzwerte für den IR-Sensor wurden erfolgreich in die Config geschrieben.")
     
 def setIRCalibrationNew():
+    """ Neue Kalibrierung der Infrarot Sensoren zur Laufzeit """
     #Neue Kalibrierung der Infrarot Sensoren zur Laufzeit
     ir = bk.Infrared()
     ir.cali_references()
@@ -383,9 +477,10 @@ doExit = False
 while doExit==False:
     """Nutzerabfrage für den gewünschten Fahrparcour"""
     print("Übersicht der Funktionen: ")
-    print("  10 Fahrprofil")
-    print("  20 Settings")
-    print("  90 Exit")
+    print("   10 Fahrprofil")
+    print("   20 Settings")
+    print("   90 Exit")
+    print("  100 Hilfe")
     TopMenu = int(input("Was wollen wir machen:"))
     if TopMenu == 10: #Fahrprofile
         while doExit==False:
@@ -411,7 +506,7 @@ while doExit==False:
         doExit = False
     elif TopMenu == 20: #Settings
         while doExit==False:
-            SetID = int(input("Welche einstellungen sollen durchgeführt werden: 10 = formatchanger; 20 = Kalibrierung IR; 30 = Refernezierung setzen"))
+            SetID = int(input("Welche einstellungen sollen durchgeführt werden: 10 = formatchanger; 20 = Kalibrierung IR; 90 = Exit"))
             if SetID == 10: 
                 FormatChanger()
             elif SetID == 20:
@@ -423,5 +518,9 @@ while doExit==False:
         doExit = False
     elif TopMenu == 90: #Exit
         doExit = True
+    elif TopMenu == 100: #Hilfe
+        print(os.path.dirname(os.path.realpath(__file__)))
+        localDir = os.path.dirname(os.path.realpath(__file__))
+        webbrowser.open(localDir + "/html/BaseCar.py.html")
     else:
         print("Ungueltige Eingabe")
